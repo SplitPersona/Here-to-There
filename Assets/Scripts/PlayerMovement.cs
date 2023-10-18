239f8706979e53199f7387af8ab29532125da7b0
SplitPersona <stephenck0203@gmail.com>
2023-10-18 21:36:54 +0100

    Added basic movement and collosion for the platforms

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f; 
+    public float jumpHeight = 7f; 
+    private Rigidbody2D body;
+    //private Animator anim; 
+    private bool grounded; 
+    private bool facingRight = true; 
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput =  Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        //anim.SetBool("walk",horizontalInput!=0);
+        
+        if((horizontalInput>0 && !facingRight)|| (horizontalInput<0&& facingRight))
+        {
+            Flip();
+        }
+
+        if(Input.GetKey(KeyCode.Space)&& grounded)
+        {
+        Jump();
+        }
+    }
+
+    private void Jump(){
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        //anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Ground"))
+        grounded = true;
+    }
+    private void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight; 
+    }
+
+
+}
